3d2c866346e996933d0e3a96796fcaaa578ddd20
dylancrawte <dylancrawte@hotmail.com>
2024-09-20 16:56:20 +0100

    initial check in

--- day1.py
+++ day1.py
@@ -1,1 +1,12 @@
+total = []
+
+with open('input.txt', 'r') as file: #opens the file
+    for line in file: #for each line in the file
+        digits = [char for char in line if char.isdigit()] #iterates through each character, adds any digits to list
+        first_digit = digits[0] 
+        last_digit = digits[-1]
+        number = int(first_digit + last_digit) #concatenates the first and last digit
+        total.append(number) #appends to total list
+        print(total)
+#part 1 complete
 
