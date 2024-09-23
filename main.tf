a7650507676c4ac075a10e5f0eb0157370929243
dylancrawte <dylancrawte@hotmail.com>
2024-09-23 10:33:22 +0100

    Created tf and yml files to create s3 bucket and lambda function

---
+++ main.tf
@@ -0,0 +1,46 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-unique-bucket-name"
+}
+
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda_exec_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
+  ]
+}
+
+resource "aws_lambda_function" "my_lambda" {
+  function_name = "my_lambda_function"
+  role          = aws_iam_role.lambda_exec_role.arn
+  handler       = "day1.lambda_handler"
+  runtime       = "python3.8"
+  filename      = "day1.zip"
+
+  source_code_hash = filebase64sha256("day1.zip")
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.my_bucket.bucket
+}
+
+output "lambda_function_name" {
+  value = aws_lambda_function.my_lambda.function_name
+}
