2dde4882ef1e86016bee16d454e82d79b87a72cd
dylancrawte <dylancrawte@hotmail.com>
2024-09-24 14:23:21 +0100

    changed day1.py to be lambda version of python code

--- day1.py
+++ day1.py
@@ -1,14 +1,37 @@
-total = []
-test = []
+import boto3
+import os
 
-with open('input.txt', 'r') as file: #opens the file
-    for line in file: #for each line in the file
-        digits = [char for char in line if char.isdigit()] #iterates through each character, adds any digits to list
-        first_digit = digits[0] 
-        last_digit = digits[-1]
-        number = int(first_digit + last_digit) #concatenates the first and last digit
-        total.append(number) #appends to total list
-        print(total)
-#part 1 completed last week and part 2 to be completed
+s3 = boto3.client('s3')
 
+def lambda_handler(event, context):
+    bucket_name = 'advent-of-code-day1 '
+    input_key = 'input.txt'
+    output_key = 'output.txt'
+    
+    # Download the file from S3
+    s3.download_file(bucket_name, input_key, '/tmp/input.txt')
+    
+    total = []
+    test = []
+
+    with open('/tmp/input.txt', 'r') as file: #opens the file
+        for line in file: #for each line in the file
+            digits = [char for char in line if char.isdigit()] #iterates through each character, adds any digits to list
+            first_digit = digits[0] 
+            last_digit = digits[-1]
+            number = int(first_digit + last_digit) #concatenates the first and last digit
+            total.append(number) #appends to total list
+    
+    # Write the result to a new file
+    with open('/tmp/output.txt', 'w') as file:
+        for number in total:
+            file.write(f"{number}\n")
+    
+    # Upload the new file to S3
+    s3.upload_file('/tmp/output.txt', bucket_name, output_key)
+    
+    return {
+        'statusCode': 200,
+        'body': 'File processed and uploaded successfully'
+    }
 
