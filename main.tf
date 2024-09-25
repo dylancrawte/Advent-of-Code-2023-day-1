46ee28a125ab4dd3207c57e886aeeddc4f49966d
Dylan <151791929+dylancrawte@users.noreply.github.com>
2024-09-25 13:40:28 +0100

    deleting irrelevant code section

--- main.tf
+++ main.tf
@@ -71,25 +71,3 @@ resource "aws_iam_role_policy" "lambda_s3_policy" {
   })
 }
 
-resource "aws_iam_role_policy" "lambda_s3_policy" {
-  name = "lambda_s3_policy"
-  role = aws_iam_role.lambda_role.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:ListBucket"w
-        ]
-        Resource = [
-          "arn:aws:s3:::advent-of-code-day",
-          "arn:aws:s3:::advent-of-code-day/*"
-        ]
-      }
-    ]
-  })
-}
