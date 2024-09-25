59c67ce35f9eaecb7c615c665a83851ba349f03d
Dylan <151791929+dylancrawte@users.noreply.github.com>
2024-09-25 11:12:55 +0100

    updated permissions

--- lambda/main.tf
+++ lambda/main.tf
@@ -24,6 +24,7 @@ resource "aws_iam_role" "lambda_exec_role" {
 
   managed_policy_arns = [
     "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
+    "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess",  // Add this line
   ]
 }
 
@@ -47,3 +48,27 @@ output "lambda_function_name" {
 #construct athena instance
 #construct actual database and table inside athena
 this is an obvious change
+
+// Add this new resource
+resource "aws_iam_role_policy" "lambda_s3_policy" {
+  name = "lambda_s3_policy"
+  role = aws_iam_role.lambda_exec_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:ListBucket",
+          "s3:PutObject"
+        ]
+        Resource = [
+          "arn:aws:s3:::advent-of-code-day",
+          "arn:aws:s3:::advent-of-code-day/*"
+        ]
+      }
+    ]
+  })
+}
