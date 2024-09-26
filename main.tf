39b8e855e4cc6211c3cce043c902f2f669535c49
Dylan <151791929+dylancrawte@users.noreply.github.com>
2024-09-26 11:46:49 +0100

    added local variable for sql in main.tf

--- main.tf
+++ main.tf
@@ -28,6 +28,22 @@ resource "aws_iam_role" "lambda_exec_role" {
   ]
 }
 
+locals {
+  sql_script = <<EOF
+-- Create an external table that points to the S3 bucket
+CREATE EXTERNAL TABLE IF NOT EXISTS advent_of_code_output (
+    line STRING
+)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY '\n'
+STORED AS TEXTFILE
+LOCATION 's3://advent-of-code-day/';
+
+-- Query the data from the external table
+SELECT line
+FROM advent_of_code_output;
+}
+
 resource "aws_lambda_function" "day1_lambda" {
   function_name    = "advent-of-code-2023-day1"
   role             = aws_iam_role.lambda_exec_role.arn
@@ -39,7 +55,7 @@ resource "aws_lambda_function" "day1_lambda" {
   environment {
     variables = {
       S3_BUCKET     = aws_s3_bucket.my_bucket.bucket
-      SQL_SCRIPT    = file("present_data.sql")
+      SQL_SCRIPT    = local.sql_script
       ATHENA_DB     = aws_athena_database.advent_database.name
       ATHENA_OUTPUT = "s3://${aws_s3_bucket.my_bucket.bucket}/athena_results/"
     }
@@ -57,7 +73,7 @@ resource "aws_lambda_function" "main_lambda" {
   environment {
     variables = {
       S3_BUCKET     = aws_s3_bucket.my_bucket.bucket
-      SQL_SCRIPT    = file("present_data.sql")
+      SQL_SCRIPT    = local.sql_script
       ATHENA_DB     = aws_athena_database.advent_database.name
       ATHENA_OUTPUT = "s3://${aws_s3_bucket.my_bucket.bucket}/athena_results/"
     }
